7ee2febf0a82bf3a1ef25202b33eb52e97090cd3
Cherri Nguyen <cherry@cherrytrees.mydomain>
2018-05-04 16:46:34 +0300

    debugged

--- RPIs/follower_test.py
+++ RPIs/follower_test.py
@@ -1,43 +1,41 @@
 from car_control import car_controller
 from wifi_connect import wifi_connector
+import sys
 
 ##commands
-#STOP = 0
-#START = 1
-#FASTER = 2
-#SLOWER = 3
-#RIGHT = 4
-#LEFT = 5
-#BACK = 6
-#WRITE = 7
-#READ = 8
+STOP = 0
+START = 1
+FASTER = 2
+SLOWER = 3
+RIGHT = 4
+LEFT = 5
+BACK = 6
+WRITE = 7
+READ = 8
 
 def main():
 	
-	car2 = car_controller(2)
-	server = wifi_connector(2)
+    car2 = car_controller(2)
+    server = wifi_connector(2,'')
 
-	while 1:
-	    msg = server.receive_from_client()
-	    cmd = int(msg)
+    while 1:
+        msg = server.receive_from_client()
+        cmd = int(msg)
 	    
-	    if cmd == STOP:
-	        print('Client sent: STOP')
-	        car2.run_stop()
-	        server.sent_to_client('Server replied: STOP')
-	    elif cmd == START:
-	        print('Client sent: START')
-	        car2.run_start()
-	        server.sent_to_client('Server replied: START')	        
-	    elif cmd == BACK:
-	        print('Client sent: BACK')
-	        car2.run_back()
-	        server.sent_to_client('Server replied: BACK')
-	    else:
-	        print('Client sent: ', msg)
-	
-
-
+        if cmd == STOP:
+            print('Client sent: STOP')
+            car2.run_stop()
+            server.send_to_client('Server replied: STOP')
+        elif cmd == START:
+            print('Client sent: START')
+            car2.run_start()
+            server.send_to_client('Server replied: START')	        
+        elif cmd == BACK:
+            print('Client sent: BACK')
+            car2.run_back()
+            server.send_to_client('Server replied: BACK')
+        else:
+            print('Client sent: ', msg)
 
 if __name__ == "__main__":
     sys.exit(main())
--- RPIs/wifi_connect.py
+++ RPIs/wifi_connect.py
@@ -7,16 +7,16 @@ SERVER = 2
 
 class wifi_connector:
     
-    def __init__(self, id):
+    def __init__(self, id, host):
         self.__id = id
         if self.__id == CLIENT:           
-            HOST = '192.168.1.102' # server IP or Hostname (found at terminal: $ hostname -I)
+            HOST = host # server IP or Hostname (found at terminal: $ hostname -I)
             PORT = 22000 # Pick an open Port (1000+ recommended), must match the server port
             self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.__client.connect((HOST,PORT))
-		    print('Client is connected to: ', HOST)		    
-	    else if self.__id == SERVER:
-	        HOST = '' # should be left empty
+	    print('Client is connected to: ' + HOST)		    
+	elif self.__id == SERVER:
+	    HOST = host # should be left empty ''
             PORT = 22000 # Pick an open Port (1000+ recommended), must match the client port
             self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             try:
@@ -24,16 +24,15 @@ class wifi_connector:
             except socket.error as msg:
                 print(msg)
             self.__server.listen(1) #allows one connection at a time
-            self.__conn, address = server.accept()
+            self.__conn, address = self.__server.accept()
             print('Server is connected to: ' + address[0] + ':' + str(address[1]))
-		else:
-            print('Invalid id: ', self.__id, ' => No connection')
+
 	        
     def __del__(self):
         if self.__id == CLIENT:
             self.__client.close()
             print('Client closed connection!')
-        else if self.__id == SERVER:
+        elif self.__id == SERVER:
             self.__conn.close()
             ##self.__server.close()
             print('Server closed connection!')
@@ -42,11 +41,11 @@ class wifi_connector:
            
     def send_to_server(self, cmd): # cmd in string
         self.__client.send(str.encode(cmd))
-        return -1;
+        return 1;
         
     def receive_from_server(self):
         raw = self.__client.recv(1024)
-		msg = raw.decode('utf-8')
+	msg = raw.decode('utf-8')
         return msg; # return string
     
     ## server ##
@@ -54,14 +53,14 @@ class wifi_connector:
     def receive_from_client(self):
         raw = self.__conn.recv(1024)
         msg = raw.decode('utf-8')
-		cmd = str.encode(msg)
+	cmd = str.encode(msg)
         ##data = msg.split()
         ##r = int(str.encode(data[1]))
         ##s = int(str.encode(data[2]))
         ##m = int(str.encode(data[3]))
         return cmd # return string
         
-    def send_to_client(msg): # msg in string
+    def send_to_client(self, msg): # msg in string
         self.__conn.send(str.encode(msg))
-        return -1;
+        return 1;
 
